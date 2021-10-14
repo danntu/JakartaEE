290b8608b4fdd72eb3f50cd36a115f7c8b705829
Danik <dan_ntu@mail.ru>
2021-10-14 21:48:23 +0600

    Добавлены во время лекции

---
+++ src/test/java/kz/iitu/jakartaee/cdi/cashier/CashierServletTest.java
@@ -0,0 +1,14 @@
+package kz.iitu.jakartaee.cdi.cashier;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashierServletTest {
+
+
+    @Test
+    void doGet() {
+        assertEquals("S", "S");
+    }
+}
